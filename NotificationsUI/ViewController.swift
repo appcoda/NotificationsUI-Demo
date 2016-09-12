21d55f37a178027be1d197c37faf7002c99aa14b
Pranjal Satija <pranjal@ephemeraapp.xyz>
2016-09-12 18:00:54 -0400

    Initial Commit

---
+++ NotificationsUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationsUI
+//
+//  Created by Pranjal Satija on 9/12/16.
+//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
